5791f0e73f4bff7aa9fa499217d125096734004c
jmomachado <joao.machado1@me.com>
2018-02-13 01:52:27 -0200

    revisado

--- Exercícios aula dia 08-02-2018/exer3.cpp
+++ Exercícios aula dia 08-02-2018/exer3.cpp
@@ -4,11 +4,25 @@
 int main (){
 	
 int vet[2][3];
-
+int a=1;
 for (int x=0; x<=1; x++){
 	for (int y=0; y<=2; y++){
-    	scanf("%d", &vet[x][y]);
-	}
+		vet[x][y]=a;
+		if (a<5){
+		a=a+1;
+		}
+		if (a>=4){
+			if(a>=10){
+				a=a+10;
+			}
+//			if (a=4){
+//				a=10;
+			else{
+//			(a>=10);
+			a=10;
+			}
+		}
+	}		
 }
 system("cls");
 printf("\n");
