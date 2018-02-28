3dffd6a897ee2c8d083345906db15b2cb90ae94d
jmomachado <joao.machado1@me.com>
2018-02-28 19:28:49 -0300

    commit

--- Exercícios aula dia 21-02-2018/exer1.cpp
+++ Exercícios aula dia 21-02-2018/exer1.cpp
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include <locale.h>
 
 int main() { 
+setlocale(LC_ALL,"ptb");
 
 int x, *p; 
 p = &x; 
 x = 10; 
 
-printf("%d", *p);
+printf("Conteúdo da variavél X: %d\n", x);
+printf("Conteúdo da variavél apontada pelo ponteiro: %d\n\n", *p);
+
+printf("Endereço da variavél X: %p\n", x);
+printf("Endereço da variavél p: %p\n", p);
 
 }
